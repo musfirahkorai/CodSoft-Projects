033df8301f3a179eca29c84129fd72277bddf007
Musfira Murtaza <120357757+musfiramurtaza@users.noreply.github.com>
2023-10-22 10:52:03 +0500

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,64 @@
+import math
+
+# Function to perform addition
+def add(x, y):
+    return x + y
+
+# Function to perform subtraction
+def subtract(x, y):
+    return x - y
+
+# Function to perform multiplication
+def multiply(x, y):
+    return x * y
+
+# Function to perform division
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+# Function to perform custom operation
+def custom_operation(expression):
+    try:
+        result = eval(expression)
+        return result
+    except:
+        return "Invalid expression"
+
+# Main function
+def main():
+    print("Simple Calculator")
+    print("Operations:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Custom Operation")
+
+    # Input from the user
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            result = add(num1, num2)
+        elif choice == '2':
+            result = subtract(num1, num2)
+        elif choice == '3':
+            result = multiply(num1, num2)
+        elif choice == '4':
+            result = divide(num1, num2)
+
+        print("Result:", result)
+    elif choice == '5':
+        expression = input("Enter a mathematical expression: ")
+        result = custom_operation(expression)
+        print("Result:", result)
+    else:
+        print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
